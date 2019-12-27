a57c165e3eaf6a826f113d111642cd9abcef6901
red <redsubmarine@yahoo.com>
2019-12-28 00:57:11 +0900

    xlsx 파싱 테스트.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const parse = require('csv-parse/lib/sync')
 const fs = require('fs')
 
@@ -8,3 +9,16 @@ records.forEach((r, i) => {
     console.log(i, r)
 })
 
+*/
+const xlsx = require('xlsx')
+const workbook = xlsx.readFile('xlsx/data.xlsx')
+
+const ws = workbook.Sheets.영화목록
+// console.log(Object.keys(workbook.Sheets))
+// console.log(ws)
+const records = xlsx.utils.sheet_to_json(ws)
+console.log(records)
+
+for (const [i, r] of records.entries()) {
+    console.log(i, r.제목, r.링크)
+}
