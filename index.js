5de3e469a23b79cef03c8e8cb450d66062c23a8a
red <redsubmarine@yahoo.com>
2019-12-28 21:56:06 +0900

    xlsx 시트 파싱 more.

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const xlsx = require('xlsx')
+const axios = require('axios')      // ajax
+const cheerio = require('cheerio')  // html parser
+
 const workbook = xlsx.readFile('xlsx/data.xlsx')
-
 const ws = workbook.Sheets.영화목록
-const records = xlsx.utils.sheet_to_json(ws)
-console.log(records)
+ws['!ref'] = ws['!ref'].split(':')
+    .map((v, i) => (i === 0) ? 'A2' : v)
+    .join(':')
 
-for (const [i, r] of records.entries()) {
-    console.log(i, r.제목, r.링크)
+const records = xlsx.utils.sheet_to_json(ws, { header: 'A' })
+// records.shift()
+
+
+const crawler = async () => {
+    await Promise.all(records.map( async (r) => {
+        const response = await axios.get(r.B)
+        if (response.status === 200) {
+            const html = response.data
+
+            const $ = cheerio.load(html)
+            const text = $('.score.score_left .star_score').text()
+
+            console.log(r.A, '평점', text.trim())
+        }
+    }))
 }
+
+crawler()
