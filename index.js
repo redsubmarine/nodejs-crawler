eb2d474233929605ec592cf21906c63ba393d3bf
red <redsubmarine@yahoo.com>
2019-12-28 15:24:14 +0900

    prettier, axios, cheerio 추가.

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-/*
-const parse = require('csv-parse/lib/sync')
-const fs = require('fs')
-
-const csv = fs.readFileSync('csv/data.csv')
-const records = parse(csv.toString('utf-8'))
-
-records.forEach((r, i) => {
-    console.log(i, r)
-})
-
-*/
 const xlsx = require('xlsx')
 const workbook = xlsx.readFile('xlsx/data.xlsx')
 
 const ws = workbook.Sheets.영화목록
-// console.log(Object.keys(workbook.Sheets))
-// console.log(ws)
 const records = xlsx.utils.sheet_to_json(ws)
 console.log(records)
 
