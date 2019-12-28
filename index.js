d93aff4b539c80e48e1e925a60f02897f3355b97
red <redsubmarine@yahoo.com>
2019-12-29 00:25:08 +0900

    csv-stringify 로 평점 데이터를 csv로 변환 작업.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const parse = require('csv-parse/lib/sync')
 const fs = require('fs')
 const puppeteer = require('puppeteer')
+const stringify = require('csv-stringify/lib/sync')
 
 const csv = fs.readFileSync('csv/data.csv')
 const records = parse(csv.toString('utf-8'))
 
 const crawler = async () => {
+    const result = []
     const browser = await puppeteer.launch({ headless: process.env.NODE_ENV === 'production' })
     await Promise.all(records.map(async (r, i) => {
         const page = await browser.newPage()
@@ -14,11 +16,18 @@ const crawler = async () => {
         if (scoreEl) {
             const text = await page.evaluate(tag => tag.textContent, scoreEl)
             console.log(r[0], '', text.trim())
+            result[i] = [r[0], r[1], text.trim()]
         }
 
         await page.close()
     }))
     await browser.close()
+
+    const str = stringify(result)
+    console.log('', result)
+    console.log('1', str)
+    fs.writeFileSync('csv/result.csv', str)
+
 }
 
 try {
