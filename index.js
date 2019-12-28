1c92cfb019e6893bc8edd1c8a81008ce9831e9cf
red <redsubmarine@yahoo.com>
2019-12-28 22:50:28 +0900

    평점 가져와서 xlsx 에 쓰기.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const xlsx = require('xlsx')
 const axios = require('axios')      // ajax
 const cheerio = require('cheerio')  // html parser
+const add_to_sheet = require('./add_to_sheet')
 
 const workbook = xlsx.readFile('xlsx/data.xlsx')
 const ws = workbook.Sheets.영화목록
@@ -13,7 +14,8 @@ const records = xlsx.utils.sheet_to_json(ws, { header: 'A' })
 
 
 const crawler = async () => {
-    await Promise.all(records.map( async (r) => {
+    add_to_sheet(ws, 'C1', 's', '평점')
+    await Promise.all(records.map( async (r, i) => {
         const response = await axios.get(r.B)
         if (response.status === 200) {
             const html = response.data
@@ -22,8 +24,11 @@ const crawler = async () => {
             const text = $('.score.score_left .star_score').text()
 
             console.log(r.A, '평점', text.trim())
+            const newCell = `C${ i + 2 }`
+            add_to_sheet(ws, newCell, 'n', parseFloat(text.trim()))
         }
     }))
+    xlsx.writeFile(workbook, 'xlsx/result.xlsx')
 }
 
 crawler()
