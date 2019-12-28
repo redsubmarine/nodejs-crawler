a2298c518e57afbce833df3c85a7ecf552e74c13
red <redsubmarine@yahoo.com>
2019-12-29 00:50:39 +0900

    조금더 개선한 버전.

--- index.js
+++ index.js
@@ -12,20 +12,26 @@ const crawler = async () => {
     await Promise.all(records.map(async (r, i) => {
         const page = await browser.newPage()
         await page.goto(r[1])
-        const scoreEl = await page.$('.score.score_left .star_score')
-        if (scoreEl) {
-            const text = await page.evaluate(tag => tag.textContent, scoreEl)
-            console.log(r[0], '', text.trim())
-            result[i] = [r[0], r[1], text.trim()]
-        }
+
+        const text = await page.evaluate(() => {
+            const score = document.querySelector('.score.score_left .star_score')
+            if (score) {
+                return score.textContent
+            }
+        })
+        result[i] = [r[0], r[1], text.trim()]
+
+        // const scoreEl = await page.$('.score.score_left .star_score')
+        // if (scoreEl) {
+        //     const text = await page.evaluate(tag => tag.textContent, scoreEl)
+        //     result[i] = [r[0], r[1], text.trim()]
+        // }
 
         await page.close()
     }))
     await browser.close()
 
     const str = stringify(result)
-    console.log('', result)
-    console.log('1', str)
     fs.writeFileSync('csv/result.csv', str)
 
 }
